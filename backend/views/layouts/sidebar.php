29e1d6c5c21d43ff3f209a6db6768fca62c05a23
FrN04 <114093821+FranciscoSousa04@users.noreply.github.com>
2024-11-22 01:53:46 +0000

    Alterações na sidebar

--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -1,3 +1,8 @@
+<?php
+
+use yii\helpers\Html;
+
+?>
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
@@ -10,57 +15,29 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="<?=$assetDir?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="<?=$assetDir?>/img/AdminLTELogo.png" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
-            </div>
-        </div>
-
-        <!-- SidebarSearch Form -->
-        <!-- href be escaped -->
-        <!-- <div class="form-inline">
-            <div class="input-group" data-widget="sidebar-search">
-                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-                <div class="input-group-append">
-                    <button class="btn btn-sidebar">
-                        <i class="fas fa-search fa-fw"></i>
-                    </button>
+                <div class="info">
+                    <a <?= Html::a(\Yii::$app->user->identity->username, ['profile/view', 'id_user' => Yii::$app->user->getId()]) ?>
                 </div>
             </div>
-        </div> -->
+        </div>
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <?php
             echo \hail812\adminlte\widgets\Menu::widget([
                 'items' => [
-                   /* [
-                        'label' => 'Starter Pages',
-                        'icon' => 'tachometer-alt',
-                        'badge' => '<span class="right badge badge-info">2</span>',
-                        'items' => [
-                            ['label' => 'Active Page', 'url' => ['site/index'], 'iconStyle' => 'far'],
-                            ['label' => 'Inactive Page', 'iconStyle' => 'far'],
-                        ]
-                    ],*/
-                    [
-                        'label' => 'Perfil',
-                        'icon' => 'th',
-                        'badge' => '<span class="right badge badge-danger"></span>',
-                        'url' => '.../site/perfil.php', // Define o caminho para a página perfil
-                    ],
                     ['label' => 'Yii2 PROVIDED', 'header' => true],
                     ['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],
                     ['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
-                    ['label' => 'Debug', 'icon' => 'bug', 'url' => ['/debug'], 'target' => '_blank'],
 
                     ['label' => 'Zona Cliente', 'header' => true],
-                    ['label' => 'Contas', 'url' => (['user/index'])],
+                    ['label' => 'Users', 'url' => (['user/index'])],
 
                     ['label' => 'Zona Funcionário', 'header' => true],
-                    ['label' => 'Contas'],
-                    ['label' => 'Adicionar Funcionário '],
+                    ['label' => 'Funcionários', 'url' => ['funcionario/index']],
 
                     ['label' => 'Zona Artigos', 'header' => true],
                     ['label' => 'Artigos', 'url' => ['artigos/index']],
@@ -68,12 +45,8 @@
                     ['label' => 'Iva', 'url' => ['iva/index']],
 
                     ['label' => 'Zona Fornecedor', 'header' => true],
-                    ['label' => 'Contas', 'url' => ['fornecedor/index']],
+                    ['label' => 'Fornecedores', 'url' => ['fornecedor/index']],
 
-                    ['label' => 'LABELS', 'header' => true],
-                    ['label' => 'Important', 'iconStyle' => 'far', 'iconClassAdded' => 'text-danger'],
-                    ['label' => 'Warning', 'iconClass' => 'nav-icon far fa-circle text-warning'],
-                    ['label' => 'Informational', 'iconStyle' => 'far', 'iconClassAdded' => 'text-info'],
                 ],
             ]);
             ?>
